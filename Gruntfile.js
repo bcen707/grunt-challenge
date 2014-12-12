f8e90e92f6cfcfaae1b73bfc3b3dd86e6623996e
bcen707 <czarsalad707@gmail.com>
2014-12-11 16:09:50 -0800

    grunt not adding module folders

---
+++ Gruntfile.js
@@ -0,0 +1,43 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+	  options: {
+	    // the banner is inserted at the top of the output
+	    banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+	  },
+	  dist: {
+	    files: {
+	      './js/output.min.js': ['./js/signup.js', './js/us-states.js']
+	    }
+	  }
+	},
+	jshint: {
+	// define files to lint	
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    cssmin: {
+	  minify: {
+	    src: './css/main.css',
+	    dest: './css/main.min.css'
+	  }
+	}
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['jshint', 'uglify', 'cssmin']);
+};
